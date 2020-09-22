6b7cffe02c9e009c6ca6dbd6addfee21f042130b
adityaeka26 <aditya.eka409@gmail.com>
2020-09-22 12:29:04 +0700

    Add Author model

---
+++ models/Author.js
@@ -0,0 +1,23 @@
+const mongoose = require('mongoose')
+const timestamp = require('mongoose-timestamp')
+
+const AuthorSchema = new mongoose.Schema({
+    name: {
+        type: String,
+        required: true 
+    },
+    gender: {
+        type: String,
+        required: true 
+    },
+    books: [{
+        type: mongoose.Schema.Types.ObjectId,
+        ref: "Book"
+    }]
+})
+
+AuthorSchema.plugin(timestamp)
+
+const Author = mongoose.model('Book', AuthorSchema)
+
+module.exports = Author
