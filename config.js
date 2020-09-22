7ef5f032816d4edf6a5606034fd4eedca1fb7370
adityaeka26 <aditya.eka409@gmail.com>
2020-09-22 18:59:39 +0700

    Update configuration file

--- config.js
+++ config.js
@@ -1,7 +1,7 @@
 module.exports = {
-    ENV: process.env.NODE_ENV || 'development',
-    PORT: process.env.PORT || 3000,
-    URL: process.env.BASE_URL || 'http://localhost:3000',
-    MONGODB_URI: process.env.MONGODB_URI || 'mongodb://localhost:27017/booksdb',
-    JWT_SECRET: process.env.JWT_SECRET || 'secret1'
+    ENV: process.env.BOOKSAPP_NODE_ENV || 'development',
+    PORT: process.env.BOOKSAPP_PORT || 3000,
+    URL: process.env.BOOKSAPP_BASE_URL || 'http://localhost:3000',
+    MONGODB_URI: process.env.BOOKSAPP_MONGODB_URI || 'mongodb://localhost:27017/booksdb',
+    JWT_SECRET: process.env.BOOKSAPP_JWT_SECRET || 'secret1'
 }
