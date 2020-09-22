03961b42cbc3c4123efb122f61e53dd75323f431
adityaeka26 <aditya.eka409@gmail.com>
2020-09-22 11:57:37 +0700

    Remove unnecessary codes

--- index.js
+++ index.js
@@ -1,15 +1,11 @@
 const restify = require('restify')
 const mongoose = require('mongoose')
 const config = require('./config')
-// const rjwt = require('restify-jwt-community')
 
 const server = restify.createServer()
 
 // Middleware
 server.use(restify.plugins.bodyParser())
-
-// Protect routes
-// server.use(rjwt({ secret: config.JWT_SECRET }).unless({ path: ['/auth', '/register'] }))
 
 server.listen(config.PORT, () => {
     mongoose.set('useFindAndModify', false)
