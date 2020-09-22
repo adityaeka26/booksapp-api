a78a7eb3d8e12c2efebe77aec88698a88dae7c95
adityaeka26 <aditya.eka409@gmail.com>
2020-09-22 18:11:23 +0700

    Add authorize middleware function to auth base on role

--- middleware/auth.js
+++ middleware/auth.js
@@ -1,26 +1,45 @@
 const bcrypt = require('bcryptjs')
 const mongoose = require('mongoose')
+const jwt = require('jsonwebtoken')
+const errors = require('restify-errors')
 const User = mongoose.model('User')
+const config = require('../config')
 
-module.exports.authenticate = (email, password) => {
-    return new Promise(async (resolve, reject) => {
-        try {
-            // Get user by email
-            const user = await User.findOne({ email })
-            // Match the password
-            bcrypt.compare(password, user.password, (err, isMatch) => {
-                if (err) {
-                    throw err
-                }
+module.exports = {
+    authenticate: (email, password) => {
+        return new Promise(async (resolve, reject) => {
+            try {
+                // Get user by email
+                const user = await User.findOne({ email })
+                // Match the password
+                bcrypt.compare(password, user.password, (err, isMatch) => {
+                    if (err) {
+                        throw err
+                    }
+    
+                    if (isMatch) {
+                        resolve(user)
+                    } else {
+                        reject('Authentication failed')
+                    }
+                })
+            } catch(err) {
+                reject('Authentication failed')
+            }
+        })
+    },
+    authorize: (roles) => {
+        const middleware = (req, res, next) => {
+            const usertoken = req.headers.authorization
+            const token = usertoken.split(' ')
+            const decoded = jwt.verify(token[1], config.JWT_SECRET)
 
-                if (isMatch) {
-                    resolve(user)
-                } else {
-                    reject('Authentication failed')
-                }
-            })
-        } catch(err) {
-            reject('Authentication failed')
+            const found = decoded.roles.some(r=> roles.indexOf(r) >= 0)
+            if (!found) {
+                next(new errors.UnauthorizedError('Unauthorized role'))
+            }
+            next()
         }
-    })
+        return middleware
+    }
 }
