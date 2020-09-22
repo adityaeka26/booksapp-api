334f29c5f5e7b735d70f6ecef1a86278689c19cf
adityaeka26 <aditya.eka409@gmail.com>
2020-09-22 12:30:14 +0700

    Remove unnecessary codes and add useUnifiedTopology: true option to mongoose.connect

--- index.js
+++ index.js
@@ -4,13 +4,13 @@ const config = require('./config')
 
 const server = restify.createServer()
 
-// Middleware
 server.use(restify.plugins.bodyParser())
 
 server.listen(config.PORT, () => {
     mongoose.set('useFindAndModify', false)
     mongoose.connect(config.MONGODB_URI, {
-        useNewUrlParser: true
+        useNewUrlParser: true,
+        useUnifiedTopology: true
     })
 })
 
